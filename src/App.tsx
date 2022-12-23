ad63e6801d0460d9666d8ac694e39928d342d192
emillykamile <emillykamile.contato@gmail.com>
2022-12-23 10:53:44 -0300

    Estrutura básica, adição de dependências e estilos globais

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import styles from './global.module.css';
+
+
+export function App() {
+
+
+  return (
+    <main className={styles.container}>
+      <nav className={styles.navMenu}>
+          
+      </nav>
+      <div>
+        <section>
+           {/* Input */}
+          <div>
+            {/* // Input Adicionar Nova Tarefa */}
+          </div>
+          <div>
+            {/* // Botão Criar */}
+          </div>
+        </section>
+        <div>
+          {/* // Renderizar as novas tarefas. */}
+        </div>
+      </div>
+    </main>
+     
+  )
+}
+
